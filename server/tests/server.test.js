88c04ec39913b567133b754d74b46f8ca842b5b4
Tad <ntdat369842@gmail.com>
2019-01-25 19:45:27 +0700

    Test GET method

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,10 +5,20 @@ const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 const {User} = require('./../models/user');
 
+const todos = [{
+    text: 'First test todo'
+}, {
+    text: 'Second test todo'
+}];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => {
-        done();
-    });
+    Todo.remove({})
+        .then(() => {
+            return Todo.insertMany(todos);
+        })
+        .then(() => {
+            done();
+        });
 });
 
 describe('POST /todos', () => {
@@ -29,7 +39,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.find()
+                Todo.find({text})
                     .then((todos) => {
                         expect(todos.length).toBe(1);
                         expect(todos[0].text).toBe(text);
@@ -56,7 +66,7 @@ describe('POST /todos', () => {
 
                 Todo.find()
                     .then((todos) => {
-                        expect(todos.length).toBe(0);
+                        expect(todos.length).toBe(2);
                         done();
                     })
                     .catch((err) => {
@@ -65,3 +75,15 @@ describe('POST /todos', () => {
             });
     })
 });
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+    })
+})
