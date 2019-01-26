de8a8c82ad1dbe4abc4ba717f9d796710cb9cad6
Tad <ntdat369842@gmail.com>
2019-01-26 14:29:55 +0700

    test mongo_uri

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp', {
+mongoose.connect("mongodb://sazerac:ae4Zqig2nYcHk8z@ds113375.mlab.com:13375/mongo-data" || 'mongodb://localhost:27017/TodoApp', {
     useMongoClient: true
 });
 
